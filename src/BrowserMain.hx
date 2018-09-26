f22aea825e87cac5cecefb757d58a95b795a5ebd
Andy Li <andy@onthewings.net>
2018-09-26 23:03:21 +0800

    move to server-side auth

--- src/BrowserMain.hx
+++ src/BrowserMain.hx
@@ -1,122 +1,15 @@
-import auth0.*;
 import js.*;
 import js.Browser.*;
 import js.jquery.JQuery;
 import haxe.*;
-import promhx.*;
-import jsrsasign.*;
-import jsrsasign.Global.*;
-
-typedef Session = {
-    var access_token:String;
-    var id_token:String;
-    var expires_at:Float;
-}
 
 class BrowserMain {
-    static var AUTH0_CLIENT_ID(default, never) = 'iI1IXOjJzqm2QQ6PclJ61HKwhW8QHJXz';
-    static var AUTH0_DOMAIN(default, never) = 'giffon.auth0.com';
-
     static function main():Void {
         new BrowserMain();
     }
 
-    var session(get, null):Null<Session>;
-    var webAuth(default, null):WebAuth;
-    var userName(default, null):Promise<String>;
-    var userNameDeferred(default, null):Deferred<String>;
-
     public function new():Void {
-        webAuth = new WebAuth({
-            domain: AUTH0_DOMAIN,
-            clientID: AUTH0_CLIENT_ID,
-            redirectUri: js.Browser.location.href,
-            responseType: 'token id_token',
-            scope: 'openid profile',
-            leeway: 60
-        });
-
-        userNameDeferred = new Deferred();
-        userName = userNameDeferred.promise();
-
-        userName.then(function(userName) {
-            new JQuery("span.user-name").text(userName);
-        });
-
         new JQuery(onReady);
-    }
-
-    function get_session():Null<Session> {
-        if (session != null) {
-            return session;
-        }
-
-        var localStorage = js.Browser.getLocalStorage();
-        if (localStorage == null) {
-            return null;
-        }
-        switch (localStorage.getItem("session")) {
-            case null:
-                return null;
-            case sessionStr:
-                var session = haxe.Json.parse(sessionStr);
-                if (session.expires_at < Date.now().getTime()) {
-                    return null;
-                }
-                return this.session = session;
-        }
-    }
-
-    function signIn(session:Session):Bool {
-        var pubkey = KEYUTIL.getKey(CompileTime.readFile("../auth0/giffon.cer"));
-        var alg = "RS256";
-        var isValid = JWS.verifyJWT(
-            session.id_token,
-            pubkey,
-            {
-                alg: [alg],
-                iss: ['https://${AUTH0_DOMAIN}/'],
-                aud: [AUTH0_CLIENT_ID]
-            }
-        );
-
-        if (isValid) {
-            var payloadObj = JWS.readSafeJSONString(b64utoutf8(session.id_token.split(".")[1]));
-            userNameDeferred.resolve(payloadObj.name);
-
-            new JQuery("body")
-                .addClass("signed-in")
-                .removeClass("signed-out");
-
-            this.session = session;
-
-
-            var localStorage = js.Browser.getLocalStorage();
-            if (localStorage == null) {
-                Browser.alert("Giffon requires localStorage to work.");
-                return false;
-            }
-            localStorage.setItem("session", Json.stringify(session));
-
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function signOut():Void {
-        var localStorage = js.Browser.getLocalStorage();
-        if (localStorage != null) {
-            localStorage.removeItem("session");
-        }
-
-        new JQuery("body")
-            .addClass("signed-out")
-            .removeClass("signed-in");
-        this.session = null;
-        webAuth.logout({
-            returnTo: location.origin + location.pathname
-        });
     }
 
     function onReady():Void {
@@ -135,48 +28,9 @@ class BrowserMain {
                 .addClass("beta");
         }
 
-        new JQuery(".signInBtn").click(function(evt){
-            evt.preventDefault();
-            if (localStorage == null) {
-                Browser.alert("Giffon requires localStorage to work.");
-                return;
-            }
-            webAuth.authorize({
-                connection: "facebook"
-            });
-        });
-
-        new JQuery(".signOutBtn").click(function(evt){
-            evt.preventDefault();
-            signOut();
-        });
-
-        webAuth.parseHash({}, function(err, authResult) {
-            if (err != null) {
-                console.error(err);
-                return;
-            }
-            if (authResult == null) {
-                if (session != null && signIn(session)) {
-                    return;
-                }
-            } else {
-                removeHash();
-
-                if (signIn({
-                    access_token: authResult.accessToken,
-                    id_token: authResult.idToken,
-                    expires_at: authResult.expiresIn * 1000 + Date.now().getTime()
-                })) {
-                    return;
-                };
-            }
-
-            // not signed in
-            new JQuery("body")
-                .addClass("signed-out")
-                .removeClass("signed-in");
-        });
+        if (location.hash == "#_=_") {
+            removeHash();
+        }
     }
 
     static function removeHash():Void { 
--- src/ServerMain.hx
+++ src/ServerMain.hx
@@ -1,10 +1,47 @@
 import js.Node.*;
 import js.npm.express.*;
 
+@:jsRequire("passport-auth0")
+extern class Auth0Strategy {
+    public function new(options:Dynamic, callb:Dynamic):Void;
+}
+
 class ServerMain {
+    static function createAuthRouter():Router {
+        var passport:Dynamic = require('passport');
+        var router = Express.GetRouter();
+        var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+
+        router.get('/login',
+            passport.authenticate('auth0', { scope: 'openid profile', connection: 'facebook'}),
+            function (req, res) {
+                res.redirect("/");
+            }
+        );
+
+        router.get('/callback',
+            passport.authenticate('auth0', { failureRedirect: '/login' }),
+            function(req, res:ExpressResponse) {
+                if (req.user == null) {
+                    throw 'user null';
+                }
+                res.redirect("/");
+            }
+        );
+
+        router.get('/user', ensureLoggedIn, function(req, res:ExpressResponse) {
+            res.send(haxe.Json.stringify(req.user, null, "  "));
+        });
+
+        router.get('/logout', function(req, res:ExpressResponse) {
+            req.logout();
+            res.redirect('/');
+        });
+
+        return router;
+    }
     static function main():Void {
-        var require:Dynamic = untyped __js__("require");
-        var isMain = require.main == module;
+        var isMain = (untyped __js__("require")).main == module;
 
         var app = Express.GetApplication();
 
@@ -13,12 +50,75 @@ class ServerMain {
             app.use(untyped awsServerlessExpressMiddleware.eventContext());
         }
 
-        app.set("view engine", "html");
+        app.set("view engine", "ejs");
 
         app.use(untyped new Static("www", {
             dotfiles: Ignore,
             redirect: true
         }));
+
+        var session = require("express-session");
+        var sessionStore = switch(process.env["SERVERLESS_STAGE"]) {
+            case "production" | "master":
+                var MySQLStore = require('express-mysql-session')(session);
+                MySQLStore({
+                    host: 'giffon.cluster-czhm2i8itlng.us-east-1.rds.amazonaws.com',
+                    port: 3306,
+                    user: 'giffon',
+                    password: '5Nrwr4WEGfZa',
+                    database: 'giffon_session'
+                });
+            case _:
+                null;
+        }
+        
+        app.use(untyped session({
+            secret: 'wm9Y5i7iLQHB8T7',
+            store: sessionStore,
+            cookie: {
+                secure: switch(process.env["SERVERLESS_STAGE"]) {
+                    case "production" | "master": true;
+                    case _: false;
+                }
+            },
+            resave: false,
+            saveUninitialized: false
+        }));
+
+        var passport = require('passport');
+        var strategy = new Auth0Strategy({
+                domain: 'giffon.auth0.com',
+                clientID: 'iI1IXOjJzqm2QQ6PclJ61HKwhW8QHJXz',
+                clientSecret: 'aRMIDi-6bQeRQnUG1GOqU8j2FfL9mz5bcWHn823iAZib78LsSXWpT-6MaF13CGiB',
+                callbackURL: switch(process.env["SERVERLESS_STAGE"]) {
+                    case "production": "https://giffon.io/callback";
+                    case "master": "https://master.giffon.io/callback";
+                    case _: "http://localhost:3000/callback";
+                }
+            },
+            function(accessToken, refreshToken, extraParams, profile, done) {
+                // accessToken is the token to call Auth0 API (not needed in the most cases)
+                // extraParams.id_token has the JSON Web Token
+                // profile has all the information from the user
+                return done(null, profile);
+            }
+        );
+        passport.use(strategy);
+        passport.serializeUser(function(user, done) {
+            done(null, user);
+        });
+        passport.deserializeUser(function(user, done) {
+            done(null, user);
+        });
+        app.use(untyped passport.initialize());
+        app.use(untyped passport.session());
+        app.use(untyped createAuthRouter());
+        app.get("/", function(req, res) {
+            res.render("index", {
+                signInStatus: req.user == null ? "signed-out" : "signed-in",
+                userName: req.user == null ? null : req.user.displayName
+            });
+        });
 
         module.exports.app = app;
 
