b7695ea9f7abd37b2562374d2d0463a3dd803784
Andy Li <andy@onthewings.net>
2018-10-02 11:51:46 +0800

    restructure template and tweak how beta works

--- src/ServerMain.hx
+++ src/ServerMain.hx
@@ -15,7 +15,7 @@ class ServerMain {
     static var SERVERLESS_STAGE(default, never):ServerlessStage = process.env["SERVERLESS_STAGE"];
     static var canonicalBase(default, never) = "https://giffon.io";
 
-    static function ensureLoggedIn(req:ExpressRequest, res:ExpressResponse, next:Dynamic):Void {
+    static function ensureLoggedIn(req:Request, res:Response, next:Dynamic):Void {
         if ((untyped req.user) == null) {
             res.redirect("/");
         } else {
@@ -42,6 +42,7 @@ class ServerMain {
             redirect: true
         }));
 
+        //auth
         app.use(function(req, res, next) {
             if (req == null || req.cookies == null || req.cookies.id_token == null) {
                 next();
@@ -65,30 +66,72 @@ class ServerMain {
             }
             next();
         });
-        app.get("/", function(req, res) {
+
+        //check beta
+        app.use(function(req:Request, res:Response, next) {
+            switch (req.query.beta) {
+                case "1":
+                    req.beta = true;
+                    res.cookie("beta", "1");
+                    next();
+                    return;
+                case "0":
+                    req.beta = false;
+                    res.clearCookie("beta");
+                    next();
+                    return;
+                case _:
+                    //pass
+            }
+            if (req.cookies != null) {
+                switch (req.cookies.beta) {
+                    case "1":
+                        req.beta = true;
+                        next();
+                        return;
+                    case _:
+                        //pass
+                }
+            }
+            req.beta = switch (SERVERLESS_STAGE) {
+                case Production: false;
+                case _: true;
+            }
+            next();
+        });
+
+        app.get("/", function(req:Request, res) {
             res.render("index", {
                 canonical: canonicalBase,
-                signInStatus: req.user == null ? "signed-out" : "signed-in",
-                userName: req.user == null ? null : req.user.name
+                bodyClasses: [],
+                isBeta: req.beta,
+                user: req.user
             });
         });
         app.get("/signin", function(req, res) {
             res.render("signin", {
                 canonical: Path.join([canonicalBase, "signin"]),
+                bodyClasses: [],
+                isBeta: req.beta,
             });
         });
-        app.get("/home", ensureLoggedIn, function(req, res:ExpressResponse) {
+        app.get("/home", ensureLoggedIn, function(req, res:Response) {
             res.render("home", {
                 canonical: Path.join([canonicalBase, "home"]),
-                userName: req.user.name,
+                bodyClasses: [],
+                isBeta: req.beta,
+                user: req.user
             });
         });
-        app.get("/user", ensureLoggedIn, function(req, res:ExpressResponse) {
+        app.get("/user", ensureLoggedIn, function(req, res:Response) {
             res.send(haxe.Json.stringify(req.user, null, "  "));
         });
-        app.get("/create-campaign", ensureLoggedIn, function(req, res:ExpressResponse) {
+        app.get("/create-campaign", ensureLoggedIn, function(req, res:Response) {
             res.render("create-campaign", {
                 canonical: Path.join([canonicalBase, "create-campaign"]),
+                bodyClasses: [],
+                isBeta: req.beta,
+                user: req.user
             });
         });
 
