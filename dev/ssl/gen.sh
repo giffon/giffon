546173ae442fdbee53011bda57fdad46efc6f505
Andy Li <andy@onthewings.net>
2018-11-04 01:54:02 +0800

    generate self-signed cert during CI

--- dev/ssl/gen.sh
+++ dev/ssl/gen.sh
@@ -1,2 +1,2 @@
-openssl req -x509 -newkey rsa:2048 -keyout keytmp.pem -out cert.pem -days 3650 -subj '/CN=localhost' -nodes
+openssl req -x509 -newkey rsa:2048 -keyout keytmp.pem -out cert.pem -days 3650 -subj "/CN=$1" -nodes
 openssl rsa -in keytmp.pem -out key.pem
