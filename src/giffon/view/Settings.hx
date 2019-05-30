bc9f5fb9a89c00c149bfda03965d8b41e8addf0e
Andy Li <andy@onthewings.net>
2019-05-30 18:11:20 +0800

    user_description can be null in the db

--- src/giffon/view/Settings.hx
+++ src/giffon/view/Settings.hx
@@ -17,7 +17,7 @@ class Settings extends Page {
         var attrs = super.bodyAttributes();
         attrs["data-current-settings"] = haxe.Json.stringify(({
             user_name: user.user_name,
-            user_description: user.user_description,
+            user_description: user.user_description == null ? "" : user.user_description,
         }:giffon.db.SettingsFormData.SettingsFormValues));
         return attrs;
     }
