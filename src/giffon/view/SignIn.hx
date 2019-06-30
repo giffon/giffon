b277b4a555916572241cfb4bde70cddf334c68a4
Andy Li <andy@onthewings.net>
2019-06-30 22:00:30 +0800

    follow Google's Sign-In Branding Guidelines
    https://developers.google.com/identity/branding-guidelines

--- src/giffon/view/SignIn.hx
+++ src/giffon/view/SignIn.hx
@@ -4,11 +4,23 @@ import react.*;
 import react.ReactMacro.jsx;
 import haxe.io.*;
 import giffon.server.ServerMain.*;
+import giffon.R.*;
 
 class SignIn extends Page {
     override function title() return 'Sign in - Giffon';
     override function path() return "signin";
     override function render() return super.render();
+
+    override function depCss() {
+        return jsx('
+            <Fragment>
+                ${super.depCss()}
+                <link rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:500"
+                />
+            </Fragment>
+        ');
+    }
 
     override function bodyClasses() return super.bodyClasses().concat(["page-signin"]);
 
@@ -17,13 +29,15 @@ class SignIn extends Page {
             <div className="h-100 row justify-content-center align-items-center">
                 <div className="card col-auto">
                     <div className="card-body">
-                        <h2 className="card-title">Sign in / Sign up</h2>
+                        <h2 className="card-title mb-4">Sign in / Sign up</h2>
 
-                        <div><a className="d-inline-block py-1" href="/signin/facebook"><i className="fab fa-facebook"></i> Facebook</a></div>
-                        <div><a className="d-inline-block py-1" href="/signin/twitter"><i className="fab fa-twitter"></i> Twitter</a></div>
-                        <div><a className="d-inline-block py-1" href="/signin/google"><i className="fab fa-google"></i> Google</a></div>
-                        <div><a className="d-inline-block py-1" href="/signin/github"><i className="fab fa-github"></i> GitHub</a></div>
-                        <div><a className="d-inline-block py-1" href="/signin/gitlab"><i className="fab fa-gitlab"></i> GitLab</a></div>
+                        <div style=${{fontFamily: "Roboto"}}>
+                            <div className="mb-2"><a className="signInBtn d-flex align-items-center border-bottom rounded-sm shadow-sm p-2" href="/signin/facebook"><img className="logo mx-1" src=${R('/images/Facebook logo 2019.svg')} alt="Facebook logo"/><span className="flex-grow-1 text-center">Sign in with Facebook</span></a></div>
+                            <div className="mb-2"><a className="signInBtn d-flex align-items-center border-bottom rounded-sm shadow-sm p-2" href="/signin/twitter"><img className="logo mx-1" src=${R('/images/Twitter_Logo_WhiteOnBlue.svg')} alt="Twitter logo"/><span className="flex-grow-1 text-center">Sign in with Twitter</span></a></div>
+                            <div className="mb-2"><a className="signInBtn d-flex align-items-center border-bottom rounded-sm shadow-sm p-2" href="/signin/google"><img className="logo mx-1" src=${R('/images/Google__G__Logo.svg')} alt="Google logo"/><span className="flex-grow-1 text-center">Sign in with Google</span></a></div>
+                            <div className="mb-2"><a className="signInBtn d-flex align-items-center border-bottom rounded-sm shadow-sm p-2" href="/signin/github"><img className="logo mx-1" src=${R('/images/github-seeklogo.com.svg')} alt="GitHub logo"/><span className="flex-grow-1 text-center">Sign in with GitHub</span></a></div>
+                            <div className="mb-2"><a className="signInBtn d-flex align-items-center border-bottom rounded-sm shadow-sm p-2" href="/signin/github"><img className="logo mx-1" src=${R('/images/gitlab-icon-rgb.svg')} alt="GitLab logo"/><span className="flex-grow-1 text-center">Sign in with GitLab</span></a></div>
+                        </div>
                     </div>
                 </div>
             </div>
