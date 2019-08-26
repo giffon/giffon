74e43cc97f722db2d3d903192cdf1c686ed29bf5
Andy Li <andy@onthewings.net>
2019-08-26 16:56:32 +0800

    use gtm and fullstory trackings

--- src/giffon/view/Page.hx
+++ src/giffon/view/Page.hx
@@ -41,7 +41,7 @@ class Page extends ReactComponent {
         <base href=${base} />
     ');
 
-    function gtag() {
+    function fullstory() {
         // only track production traffic
         switch (giffon.config.Stage.stage) {
             case Production: //pass
@@ -49,17 +49,62 @@ class Page extends ReactComponent {
         }
 
         var scriptContent = "
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-125793554-1');
+            window['_fs_debug'] = false;
+            window['_fs_host'] = 'fullstory.com';
+            window['_fs_org'] = 'NPXHJ';
+            window['_fs_namespace'] = 'FS';
+            (function(m,n,e,t,l,o,g,y){
+                if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window[\"_fs_namespace\"].');} return;}
+                g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+                o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_host+'/s/fs.js';
+                y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+                g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+                g.shutdown=function(){g(\"rec\",!1)};g.restart=function(){g(\"rec\",!0)};
+                g.log = function(a,b) { g(\"log\", [a,b]) };
+                g.consent=function(a){g(\"consent\",!arguments.length||a)};
+                g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+                g.clearUserCookie=function(){};
+            })(window,document,window['_fs_namespace'],'script','user');
         ";
         var scriptHtml = {__html: scriptContent};
         return jsx('
             <Fragment>
-                <script async=${true} src="https://www.googletagmanager.com/gtag/js?id=UA-125793554-1"></script>
                 <script dangerouslySetInnerHTML=${scriptHtml} />
             </Fragment>
+        ');
+    }
+
+    function gtmHead() {
+        // only track production traffic
+        switch (giffon.config.Stage.stage) {
+            case Production: //pass
+            case _: return null;
+        }
+
+        var scriptContent = "
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TSVCXGW');
+        ";
+        var scriptHtml = {__html: scriptContent};
+        return jsx('
+            <Fragment>
+                <script dangerouslySetInnerHTML=${scriptHtml} />
+            </Fragment>
+        ');
+    }
+
+    function gtmBody() {
+        // only track production traffic
+        switch (giffon.config.Stage.stage) {
+            case Production: //pass
+            case _: return null;
+        }
+        return jsx('
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TSVCXGW"
+            height="0" width="0" style=${{display:"none", visibility:"hidden"}}></iframe></noscript>
         ');
     }
 
@@ -208,7 +253,8 @@ class Page extends ReactComponent {
 
     function head() return jsx('
         <head>
-            ${gtag()}
+            ${gtmHead()}
+            ${fullstory()}
             <meta charSet="UTF-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -271,6 +317,7 @@ class Page extends ReactComponent {
 
     function body() return jsx('
         <body className=${bodyClasses().join(" ")} {...bodyAttributes()}>
+            ${gtmBody()}
             <div className="content">
                 <nav className="navbar navbar-light navbar-expand-md font_xs_s font_md_m">
                     <a className="navbar-brand mr-0" href="" style=${{backgroundImage: 'url(${R("/images/logo-blue.svg")})'}}>
