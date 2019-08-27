c34ca1297447b1c3dbd41d475abda224678a369b
Andy Li <andy@onthewings.net>
2019-08-27 11:19:46 +0800

    Revert "full story is loaded from gtm"
    This reverts commit 45400740d4d96762f5fac46e9f5b5bdb09a11b9f.

--- src/giffon/view/Page.hx
+++ src/giffon/view/Page.hx
@@ -41,6 +41,39 @@ class Page extends ReactComponent {
         <base href=${base} />
     ');
 
+    function fullstory() {
+        // only track production traffic
+        switch (giffon.config.Stage.stage) {
+            case Production: //pass
+            case _: return null;
+        }
+
+        var scriptContent = "
+            window['_fs_debug'] = false;
+            window['_fs_host'] = 'fullstory.com';
+            window['_fs_org'] = 'NPXHJ';
+            window['_fs_namespace'] = 'FS';
+            (function(m,n,e,t,l,o,g,y){
+                if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window[\"_fs_namespace\"].');} return;}
+                g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+                o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_host+'/s/fs.js';
+                y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+                g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+                g.shutdown=function(){g(\"rec\",!1)};g.restart=function(){g(\"rec\",!0)};
+                g.log = function(a,b) { g(\"log\", [a,b]) };
+                g.consent=function(a){g(\"consent\",!arguments.length||a)};
+                g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+                g.clearUserCookie=function(){};
+            })(window,document,window['_fs_namespace'],'script','user');
+        ";
+        var scriptHtml = {__html: scriptContent};
+        return jsx('
+            <Fragment>
+                <script dangerouslySetInnerHTML=${scriptHtml} />
+            </Fragment>
+        ');
+    }
+
     function gtmHead() {
         // only track production traffic
         switch (giffon.config.Stage.stage) {
@@ -221,6 +254,7 @@ class Page extends ReactComponent {
     function head() return jsx('
         <head>
             ${gtmHead()}
+            ${fullstory()}
             <meta charSet="UTF-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
